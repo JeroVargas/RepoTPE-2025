00a783fe2e9c9349b1cab6ab2fc02cbf8bd2091f
LianBellocchio <giulianonicolasbellocchio@gmail.com>
2025-10-15 17:35:37 -0300

    feat(login & register) implementacion inicial del session y session auth

--- app/controllers/pisos.controller.php
+++ app/controllers/pisos.controller.php
@@ -2,28 +2,31 @@
 require_once './app/models/pisos.model.php';
 require_once './app/views/templates/pisos.view.php';
 
-class PisosController{
+class PisosController
+{
     private $model;
     private $view;
-                                
-    public function __construct($res) {
+
+    public function __construct($res)
+    {
         $this->model = new PisosModel();
         $this->view = new PisosView($res->user);
     }
 
-    public function showHome(){
+    public function showHome()
+    {
         //muestro el index.phtml
         return $this->view->showHome();
     }
 
-    public function showPisos(){
-    $pisos = $this->model->getPisos();
-    $this->view->showPisos($pisos);
-}
+    public function showPisos()
+    {
+        $pisos = $this->model->getPisos();
+        $this->view->showPisos($pisos);
+    }
 
-
-    
-    public function showError($error){
+    public function showError($error)
+    {
         //muestro el error.phtml
         return $this->view->showError($error);
     }
--- router.php
+++ router.php
@@ -1,16 +1,16 @@
 <?php
-require_once 'app/libs/response.php';
-require_once 'app/middlewares/session.auth.middlewares.php';
-require_once 'app/middlewares/verify.auth.middlewares.php';
-require_once 'app/controllers/pisos.controller.php';
-require_once 'app/controllers/auth.controller.php';
+require_once './app/libs/response.php';
+require_once './app/middlewares/session.auth.middlewares.php';
+require_once './app/middlewares/verify.auth.middlewares.php';
+require_once './app/controllers/pisos.controller.php';
+require_once './app/controllers/auth.controller.php';
 
-define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
 $res = new Response();
 
-$action = 'index'; 
-if (!empty( $_GET['action'])) {
+$action = 'index';
+if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
 
@@ -26,21 +26,22 @@ switch ($params[0]) {
     case 'lista_pisos':
         $controller = new PisosController($res);
         $controller->showPisos();
-        break; 
-    
+        break;
+
     case 'login':
         $controller = new AuthController($res);
         $controller->showLogin();
-    
+        break;
+
     case 'error':
         sessionAuthMiddleware($res);
         $controller = new PisosController();
         $controller->showError("404 Page Not Found");
         break;
-        
-        default:
+
+    default:
         $controller = new PisosController();
-        $error= "404 Page Not Found";
-        $controller->showError($error); 
+        $error = "404 Page Not Found";
+        $controller->showError($error);
         break;
-    }
+}
